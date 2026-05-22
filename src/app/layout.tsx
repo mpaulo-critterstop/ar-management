686a08c16e8c28fc3e2e6b785a00f49c6b3e6691
mpaulo-critterstop <mpaulo@critterstop.com>
2026-05-22 17:00:31 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "AR Management — Critter Stopper",
+  title: "AR Management — Critter Stop",
   description: "Accounts Receivable Management",
 };
 
