27597bda3a561d38a7db4e51ffa39f364b103ee9
mpaulo-critterstop <mpaulo@critterstop.com>
2026-06-04 12:27:26 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 import { Providers } from "@/components/Providers";
 import { TopNav } from "@/components/TopNav";
 
